18486bde68b01bba00fb3aa4886a71fcda3d245c
Dale-Xu <67305318+Dale-Xu@users.noreply.github.com>
2025-08-31 18:02:57 +0800

    Update Custom_activation.py

--- Custom_activation.py
+++ Custom_activation.py
@@ -7,8 +7,8 @@ import torch.distributions as dist
 class LANReLU(nn.Module):
     def __init__(self):
         super(LANReLU, self).__init__()
-        self.sigma = nn.Parameter(torch.tensor(0.2), requires_grad=True)
-        #self.sigma = nn.Parameter(torch.tensor(0.1)*torch.rand(1)+torch.tensor(0.), requires_grad=True)
+        #self.sigma = nn.Parameter(torch.tensor(0.2), requires_grad=True)
+        self.sigma = nn.Parameter(torch.tensor(0.)*torch.rand(1)+torch.tensor(0.2), requires_grad=True)
         #self.sigma = nn.Parameter(torch.tensor(-2.), requires_grad=True)
 
     def forward(self, x, training=True):
@@ -18,4 +18,5 @@ class LANReLU(nn.Module):
         D = self.sigma
         normal_dist = dist.Normal(0, 1)
         output = x * normal_dist.cdf(x / D) + D * torch.exp(-0.5 * (x / D) ** 2) / torch.sqrt(2 * torch.tensor(math.pi))
+
         return output
